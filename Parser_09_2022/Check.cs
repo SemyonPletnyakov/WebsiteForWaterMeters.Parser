97b5e999f199fd5e21aa2510bf5443fe71846ca3
Семён Семёнович Плетняков <pletnyakov01@mail.ru>
2022-12-10 21:07:44 +0300

    4

--- Parser_09_2022/Check.cs
+++ Parser_09_2022/Check.cs
@@ -24,5 +24,6 @@ namespace Parser_09_2022
         public string UslugaName { get; set; }
         public string Price { get; set; }
         public bool PaymentStatus { get; set; }
+        public int PayId { get; set; }
     }
 }
